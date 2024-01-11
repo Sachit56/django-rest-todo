7f2508bc759bdc1593ca5eb6f46ddf15463c3af4
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-11 20:26:11 +0545

    cors error fixed

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -6,7 +6,7 @@ app_name='todo'
 urlpatterns = [
     path('',views.Home.as_view(),name='home'),
     path('create/',views.CreateView.as_view(),name='home'),
-    path('delete/<int:pk>/',views.DeleteView.as_view(),name='home'),
+    path('delete/<int:pk>',views.DeleteView.as_view(),name='home'),
 
 
 
